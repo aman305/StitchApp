ef1934ef7088937b23d137bfae52affa6ba95f9d
Neema Deshwal <neemadeshwal@Neema-Deshwal-B994s-MacBook-Air.local>
2025-09-18 17:41:14 +0530

    fixed metro configuration

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+
+// Get the default Expo Metro config
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
+
+  // Watch folders - include root node_modules and potentially packages
+  watchFolders: [
+    path.resolve(__dirname, "../../node_modules"),
+    path.resolve(__dirname, "../../packages"), // If you have shared packages
+  ],
+
+  resolver: {
+    ...defaultConfig.resolver,
+
+    // File extensions Metro should resolve
+    sourceExts: ["tsx", "ts", "js", "jsx", "json"],
+
+    // Help Metro find modules in the root node_modules
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          // First try the app's node_modules, then root
+          const appNodeModules = path.join(__dirname, "node_modules", name);
+          const rootNodeModules = path.join(
+            __dirname,
+            "../../node_modules",
+            name
+          );
+
+          if (require("fs").existsSync(appNodeModules)) {
+            return appNodeModules;
+          }
+          return rootNodeModules;
+        },
+      }
+    ),
+
+    // Ensure Metro can resolve modules from monorepo packages
+    platforms: ["ios", "android", "native", "web"],
+  },
+
+  // Transform configuration
+  transformer: {
+    ...defaultConfig.transformer,
+    // Enable Hermes for better performance
+    hermesCommand:
+      "../../node_modules/react-native/sdks/hermesc/%OS-BIN%/hermesc",
+  },
+};
